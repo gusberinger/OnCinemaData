d04b61cbd50a65350da1ce0699f841e99d4ba536
Gus Beringer <gusberinger@gmail.com>
2020-11-20 14:15:17 -0600

    Fixed the oscar detection

--- scraper.py
+++ scraper.py
@@ -10,37 +10,53 @@ class Episode:
         self.airdate = self.soup.find("h5", text = "Original Air Date").parent.p.text
         hosts = self.soup.find("h5", text="Hosts / Guests").parent.find_all("div")
         self.hosts = [item.text for item in hosts]
-        # try:
-        self.next_episode = self.soup.find("a", class_="next-ep").get("href")
-        # except Exception:
-        #     self.next_episode = None
-    def get_movies(self):
         movies = self.soup.find("div", "episode-movies").find_all("div", recursive = False)
-        movies = [Movie(soup) for soup in movies]
+        self.movies = [Movie(soup) for soup in movies]
+
+
+    def episode_csv(self):
+            return f"{self.airdate},{self.season},{self.episode}"
+
             
-
-    
-
 class Movie:
     def __init__(self, soup):
         self.soup = soup
         self.title = self.soup.find("p", class_ = "movie-title").text
         self.year = re.match(r"\d+", self.soup.find(text="Year: ").next).group()
-        print(year)
-
-class MovieReview:
-    def __init__(self, soup):
-        self.soup = soup
-        # self.author = self.find("div", class_ = "review-info").p
-        self.author = self.find("p", class="title").text
-        self.popcorn = self.find("p", class="rating-summary").text
+        self.reviews = {}
+        self.reviews['Gregg'] = {}
+        self.reviews['Tim'] = {}
+        reviews_soup = soup.find("div", class_= "row reviews").find_all("div", class_ = "review-info")
+        # go up two levels to see oscar information
+        reviews_soup = [x.parent.parent for x in reviews_soup]
         
+        # simplify author
+        for review in reviews_soup:
+            author = review.find("p", class_ = "title").text
+            if author == "Gregg's Review":
+                author = "Gregg"
+            elif author == "Tim's Review":
+                author = "Tim"
+                
+            self.reviews[author] = self.build_review_db(review)
+
+
+    def build_review_db(self, review):
+        """Build a database of information for each review"""
+        db = {}
+        review_text = review.find("p", class_ = "rating-summary").text
+        db['popcorn'] = re.match(r"\d+", review_text).group()
+        if review.find("div", class_ = "oscar-badge"):
+            db['oscar'] = True
+        else:
+            db['oscar'] = False
+
+        return db
+
+    def movie_csv(self):
+        return f"{self.title}"
 
 start = Episode("https://oncinematimeline.com/season-1/episode-1")
-print(start.airdate)
-print(start.hosts)
-print(start.next_episode)
-print(start.season)
-print(start.episode)
-start.get_movies()
+print(start.movies[0].reviews)
+print(start.movies[1].reviews)
 
