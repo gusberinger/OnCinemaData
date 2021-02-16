7776e4e29bef86145abe37025bbf4f254cfc2f31
unknown <gusberinger@gmail.com>
2021-02-16 15:36:07 -0600

    split up the scraper into 2 parsing scripts

---
+++ scripts/parse_episode_info.py
@@ -0,0 +1,66 @@
+import glob
+from bs4 import BeautifulSoup
+import requests
+import json
+import re
+import pandas as pd
+from functools import reduce
+
+with open("secrets/youtube_api.txt", "r") as f:
+    youtube_key = f.read()
+
+
+def epsiode_info(season, episode_number):
+    """Returns as a list of dictionaries all relevent information considering movies
+    and reviews of those movies in the episode."""
+    with open(f"pages/{season}-{episode_number}.html", "r") as f:
+        html = f.read()
+
+    episode = {"season": season, "episode": episode_number}
+    soup = BeautifulSoup(html, "html.parser")
+    episode["youtube_id"] = soup.find("a", text="Youtube").get("href")[32:]
+    episode["airdate"] = soup.find("h5", text = "Original Air Date").parent.p.text
+    hosts = soup.find("h5", text="Hosts / Guests").parent.find_all("div")
+    episode["hosts"] = "|".join([item.text for item in hosts])
+    episode["n_hosts"] = len(hosts)
+    youtube_api_url = f"https://www.googleapis.com/youtube/v3/videos?part=statistics&id={episode['youtube_id']}&key={youtube_key}"
+    youtube_data_all = json.loads(requests.get(youtube_api_url).content)
+    youtube_data = youtube_data_all['items'][0]['statistics']
+    
+    complete_data = episode | youtube_data
+    print(complete_data)
+    return complete_data
+
+
+def movie_info(soup):
+    """Returns all movie data, including reviews for one movie as list with item per review"""
+    title = soup.find("p", class_ = "movie-title").text
+    year = re.match(r"\d+", soup.find(text="Year: ").next).group()
+    omdb = json.loads(requests.get(omdb_url(title, year)).content)
+    omdb.pop('Ratings', None) # make it easier to join data
+
+    reviews_soup = soup.find("div", class_= "row reviews").find_all("div", class_ = "review-info")
+    reviews = [review_info(soup) for soup in reviews_soup]
+    movie_data = [omdb | review for review in reviews]
+    return movie_data
+
+def get_all_movie_info(season, episode_numer):
+    """Returns list of all movie info"""
+    with open(f"pages/{season}-{episode_number}.html", "r") as f:
+        html = f.read()
+    soup = BeautifulSoup(html, "html.parser")
+    movies_soup = soup.find("div", "episode-movies").find_all("div", recursive = False)
+    movie_data = {"season": season, "episode": episode_number}
+    movies = [movie_info(soup) for soup in movies_soup]
+    movies_flattened = reduce(lambda x, y: x + y, movies) # grab all review rows from lists
+    print(movies_flattened)
+
+
+all_episode_info = []
+for season in range(1, 3):
+    for episode_number in range(1, 11):
+        print(f"Parsing {season}-{episode_number}")
+        get_all_movie_info(season, episode_number)
+        all_episode_info.append(epsiode_info(season, episode_number))
+df = pd.DataFrame(all_episode_info)
+df.to_csv("datasets/episode_info.csv", index = False)
---
+++ scripts/parse_movie_info.py
@@ -0,0 +1,81 @@
+import pathlib
+from bs4 import BeautifulSoup
+import re
+import json
+import requests
+from functools import reduce
+import pandas as pd
+
+with open("secrets/omdb_api.txt", "r") as f:
+    omdb_key = f.read()
+
+def omdb_url(title, year):
+    """From the title and year get the url for omdb"""
+    # adjust data to match omdb
+    if title == "Les Miserables":
+        title = "Les Misérables"
+    if title == "Escape from Planet Earth":
+        year = 2012
+    elif title == "Scary Movie 5":
+        title = "Scary Movie V"
+    elif title == "No Escape (aka The Coup)":
+        title = "No Escape"
+    elif title == "The Moon and the Sun":
+        title = "The King's Daughter"
+        year = 2020
+    elif title == "Autobahn":
+        title = "Collide"
+    elif title == "The Last Full Measure":
+        year = 2019
+    elif title == "Fast & Furious 8":
+        title = "The Fate of the Furious"
+
+    url = f"http://www.omdbapi.com/?t={title.replace('&', '%26')}&y={year}&apikey={omdb_key}"
+    return url
+
+def review_info(soup):
+    review = {}
+    author_full = soup.find("p", class_ = "title").text
+    review['author'] = re.match(r"(\w+)'s Review", author_full).group(1)
+    if soup.find("p", class_ = "did-not-rate"):
+        review["popcorn"] = "NA"
+    else:
+        review_text = soup.find("p", class_ = "rating-summary").text
+        popcorn = re.match(r"\d+", review_text)
+        review["popcorn"] = popcorn.group(0)
+    review["oscar_pick"] = bool(soup.find("div", class_ = "oscar-badge"))
+    return review
+
+def movie_info(soup):
+    """Returns all movie data, including reviews for one movie as list with item per review"""
+    title = soup.find("p", class_ = "movie-title").text
+    year = re.match(r"\d+", soup.find(text="Year: ").next).group()
+    omdb = json.loads(requests.get(omdb_url(title, year)).content)
+    omdb.pop('Ratings', None) # make it easier to join data
+
+    reviews_soup = soup.find("div", class_= "row reviews").find_all("div", class_ = "review-info")
+    reviews = [review_info(soup) for soup in reviews_soup]
+    movie_data = [omdb | review for review in reviews]
+    return movie_data
+
+def get_all_movie_info(season, episode_number):
+    """Returns list of all movie info"""
+    root_path = pathlib.Path(__file__).parent.parent
+    path = str(root_path) + f"/pages/{season}-{episode_number}.html"
+    with open(path, "r") as f:
+        html = f.read()
+    soup = BeautifulSoup(html, "html.parser")
+    movies_soup = soup.find("div", "episode-movies").find_all("div", recursive = False)
+    movies = [movie_info(soup) for soup in movies_soup]
+    movies_flattened = reduce(lambda x, y: x + y, movies) # grab all review rows from lists
+    movies_complete = [{"season": season, "episode": episode_number} | data for data in movies_flattened]
+
+    return movies_complete
+
+reviews = []
+for season in range(1, 2):
+        for episode_number in range(1, 5):
+            print(f"Parsing movie reviews for {season}-{episode_number}")
+            reviews += get_all_movie_info(season, episode_number)
+reviews_df = pd.DataFrame(reviews)
+reviews_df.to_csv(str(pathlib.Path(__file__).parent.parent) + "/datasets/movie_info.csv", index = False)
