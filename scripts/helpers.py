0f75d69d14aa03e45fcb78546875f023ce99a18b
unknown <gusberinger@gmail.com>
2021-02-17 22:33:32 -0600

    moved the omdb function to helper script

---
+++ scripts/helpers.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import pathlib
+
+PROJECT_ROOT = str(pathlib.Path(__file__).parent.parent)
+
+with open(str(pathlib.Path(__file__).parent.parent) + "/secrets/omdb_api.txt", "r") as f:
+    omdb_key = f.read()
+
+def omdb_url(title, year):
+    """From the title and year get the url for omdb"""
+    # adjust data to match omdb
+    if title == "CafÃ© Society":
+        title = "Café Society"
+    if title == "Les Miserables":
+        title = "Les Misérables"
+    if title == "Escape from Planet Earth":
+        year = 2012
+    elif title == "Scary Movie 5":
+        title = "Scary Movie V"
+    elif title == "No Escape (aka The Coup)":
+        title = "No Escape"
+    elif title == "The Moon and the Sun":
+        title = "The King's Daughter"
+        year = 2020
+    elif title == "Autobahn":
+        title = "Collide"
+    elif title == "The Last Full Measure":
+        year = 2019
+    elif title == "Fast & Furious 8":
+        title = "The Fate of the Furious"
+
+    url = f"http://www.omdbapi.com/?t={title.replace('&', '%26')}&y={year}&apikey={omdb_key}"
+    return url
+
+def get_omdb_data(title, year):
+    omdb = json.loads(requests.get(omdb_url(title, year)).content)
+    omdb.pop('Ratings', None) # superfluous, and removing makes it easier to join data
+    return omdb
--- scripts/parse_movie_info.py
+++ scripts/parse_movie_info.py
@@ -1,39 +1,10 @@
 import pathlib
 from bs4 import BeautifulSoup
 import re
-import json
-import requests
+from helpers import get_omdb_data, PROJECT_ROOT
 from functools import reduce
 import pandas as pd
 
-with open(str(pathlib.Path(__file__).parent.parent) + "/secrets/omdb_api.txt", "r") as f:
-    omdb_key = f.read()
-
-def omdb_url(title, year):
-    """From the title and year get the url for omdb"""
-    # adjust data to match omdb
-    if title == "CafÃ© Society":
-        title = "Café Society"
-    if title == "Les Miserables":
-        title = "Les Misérables"
-    if title == "Escape from Planet Earth":
-        year = 2012
-    elif title == "Scary Movie 5":
-        title = "Scary Movie V"
-    elif title == "No Escape (aka The Coup)":
-        title = "No Escape"
-    elif title == "The Moon and the Sun":
-        title = "The King's Daughter"
-        year = 2020
-    elif title == "Autobahn":
-        title = "Collide"
-    elif title == "The Last Full Measure":
-        year = 2019
-    elif title == "Fast & Furious 8":
-        title = "The Fate of the Furious"
-
-    url = f"http://www.omdbapi.com/?t={title.replace('&', '%26')}&y={year}&apikey={omdb_key}"
-    return url
 
 def review_info(soup):
     review = {}
@@ -52,9 +23,7 @@ def movie_info(soup):
     """Returns all movie data, including reviews for one movie as list with item per review"""
     title = soup.find("p", class_ = "movie-title").text
     year = re.match(r"\d+", soup.find(text="Year: ").next).group()
-    omdb = json.loads(requests.get(omdb_url(title, year)).content)
-    omdb.pop('Ratings', None) # make it easier to join data
-
+    omdb = get_omdb_data(title, year)
     reviews_soup = soup.find("div", class_= "row reviews").find_all("div", class_ = "review-info")
     reviews = [review_info(soup) for soup in reviews_soup]
     movie_data = [omdb | review for review in reviews]
@@ -62,8 +31,7 @@ def movie_info(soup):
 
 def get_all_movie_info(season, episode_number):
     """Returns list of all movie info"""
-    root_path = pathlib.Path(__file__).parent.parent
-    path = str(root_path) + f"/pages/{season}-{episode_number}.html"
+    path = PROJECT_ROOT + f"/pages/{season}-{episode_number}.html"
     with open(path, "r") as f:
         html = f.read()
     soup = BeautifulSoup(html, "html.parser")
@@ -75,9 +43,9 @@ def get_all_movie_info(season, episode_number):
     return movies_complete
 
 reviews = []
-for season in range(1, 12):
+for season in range(1, 2):
         for episode_number in range(1, 11):
             print(f"Parsing movie reviews for {season}-{episode_number}")
             reviews += get_all_movie_info(season, episode_number)
 reviews_df = pd.DataFrame(reviews)
-reviews_df.to_csv(str(pathlib.Path(__file__).parent.parent) + "/datasets/movie_info.csv", index = False)
+reviews_df.to_csv(PROJECT_ROOT + "/datasets/movie_info.csv", index = False)
